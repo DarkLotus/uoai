c2ae72aab979e894d73976031beae5d6d4728b01
wim.decelle <wim.decelle@gmail.com>
2011-11-14 21:31:40 +0000

    - thiscall wrappers added : thiscall uses the ecx register to pass the thispointer (typical C++ class member method calling convention). The thiscall wrappers added are dynamically written calls that either take the this-pointer from ecx and push it on the stack or the opposite way around; so this allows calling thiscall functions as if they were C functions (this parameter on the stack) and passing C functions as thiscall-callbacks. YesNoGump test code updated to test all of this.

--- Hooks.h
+++ Hooks.h
@@ -43,5 +43,17 @@ int SetCallHook(unsigned int calladdr, pHookFunc hookfunc, unsigned int original
 */
 int SetRandomHook(unsigned int address, unsigned int stacksize, pHookFunc hookfunc);
 
+/* thiscall wrappers:	- not really hooks, but require writing x86 instructions, so belong to the same category as the hook code
+						- create_thiscall_wrapper
+								A thiscall wrapper takes the 'this' parameter from the stack and moves it into the ecx register
+								before performing the call. To do this instructions are written to backup and restore the return address before popping this -> ecx.
+								This allows wrapping thiscalls into __stdcall (*)(void *pThis, ...rest_of_parameters...) calls.
+						- create_thiscall_callback 
+								A thiscall callback is the opposite of a thiscall wrapper. This creates a chunk which takes the this-pointer from the ecx register
+								and pushes it onto the stack. Again the returnaddress is backed up and restored to do this.
+								Example of usage is when installing YesNoGump->onYes or ->onNo callbacks. Those get the pThis pointer to the yes-no-gump in ecx.
+						*/
+unsigned int create_thiscall_wrapper(unsigned int towrap);
+unsigned int create_thiscall_callback(unsigned int towrap);
 
 #endif
